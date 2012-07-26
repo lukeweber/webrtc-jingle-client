5534d80437e6df929f763248c099141b888849ad
Nick Flink <nickflink@github.com>
2012-07-26 19:42:49 +0200

    update badit script to have options for syncing and build system but only gyp is supported

--- badit.sh
+++ badit.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+#
+# badit.sh - build and debug it
+# @author: Nick Flink <nick@tuenti.com>
+##
+##  The following options are provided.
+##  -h             help. What you are reading now.
+##  -g             gyp. do the gyp build
+##  -s             sync. sync before doing the build
+#
+# default variables
+#
+BUILDSYSTEM="gyp"
+SYNCREPOS="no"
 TRUNKDIR="$( cd "$( dirname "$0" )" && pwd )"
 SRCDIR="$TRUNKDIR/src"
-
 OPENSSL_PATCH="--- a/third_party/openssl/openssl.gyp
 +++ b/third_party/openssl/openssl.gyp
 @@ -13,6 +13,8 @@
@@ -13,6 +25,15 @@ OPENSSL_PATCH="--- a/third_party/openssl/openssl.gyp
          # We do not use TLS over UDP on Chromium so far.
          'OPENSSL_NO_DTLS1',
        ],"
+#
+# helper functions
+#
+print_usage(){
+  echo "$1" >&2
+  echo -e "Usage: $0 [-h|-s|-g|]\n" >&2 
+  sed -n '/^## /s/^## //p' $0 >&2
+  exit 1
+}
 function check_return_code(){
   if [ "$1" != "0" ];then
     echo "last command failed bailing"
@@ -20,6 +41,40 @@ function check_return_code(){
     exit $1
   fi
 }
+#
+# arg parsing
+#
+validate_optarg(){
+  [[ "${OPTARG:0:1}" = "-" ]] && print_usage "-$opt: requires an argument"
+}
+
+
+
+while getopts "hgs" opt
+do
+  case $opt in
+    h ) #help
+      print_usage
+      ;;
+    g ) #gyp
+      BUILDSYSTEM="gyp"
+      ;;
+    s ) #sync
+      SYNCREPOS="yes"
+      ;;
+    : )
+      print_usage "Option -$OPTARG requires an argument."
+      ;;
+    ? )
+      if [ "${!OPTIND}" == "--help" ]
+      then
+        print_usage
+      else
+        print_usage "Invalid option: -$OPTARG"
+      fi
+      ;;
+  esac
+done
 
 pushd $TRUNKDIR;
 echo "patching gyp"
@@ -30,24 +85,29 @@ echo "CHROME_SRC=$TRUNKDIR"
 export CHROME_SRC="$TRUNKDIR"
 source "$TRUNKDIR/build/android/envsetup.sh" || { echo Fatal: Cannot load $TRUNKDIR/build/android/envsetup.sh;  exit 1; }
 
-gclient sync;
-check_return_code "$?"
+if [ "$SYNCREPOS" == "yes" ]; then
+  gclient sync;
+  check_return_code "$?"
+fi
+if [ "$BUILDSYSTEM" == "gyp" ]; then
+  gclient runhooks;
+  check_return_code "$?"
 
-gclient runhooks;
-check_return_code "$?"
+  make webrtc_jingle -j4 BUILDTYPE=Debug
+  check_return_code "$?"
 
-make webrtc_jingle -j4 BUILDTYPE=Debug
-check_return_code "$?"
+  adb uninstall com.tuenti.voice;
+  check_return_code "$?"
 
-adb uninstall com.tuenti.voice;
-check_return_code "$?"
+  adb install $TRUNKDIR/out/Debug/VoiceClient/VoiceClient-debug.apk
+  check_return_code "$?"
 
-adb install $TRUNKDIR/out/Debug/VoiceClient/VoiceClient-debug.apk
-check_return_code "$?"
+  adb shell am start -a android.intent.action.VIEW  -n com.tuenti.voice/.VoiceClientActivity
+  check_return_code "$?"
 
-adb shell am start -a android.intent.action.VIEW  -n com.tuenti.voice/.VoiceClientActivity
-check_return_code "$?"
-
-$TRUNKDIR/build/android/gdb_apk -p com.tuenti.voice -l out/Debug/obj.target/
-check_return_code "$?"
+  $TRUNKDIR/build/android/gdb_apk -p com.tuenti.voice -l out/Debug/obj.target/
+  check_return_code "$?"
+else
+  print_usage "Only gdb build system available for now"
+fi
 popd;
