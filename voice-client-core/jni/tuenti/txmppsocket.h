c5764d82adac1bb30673887b707889899d0fbfe1
lukeweber <luke.weber@gmail.com>
2012-08-08 17:06:23 +0200

    Revert "* Reverting explicit declaration of xmppsocket"
    This reverts commit 63ee8def0764b9a59cf3f69bc8a175d528e11108.

--- voice-client-core/jni/tuenti/txmppsocket.h
+++ voice-client-core/jni/tuenti/txmppsocket.h
@@ -49,7 +49,7 @@ extern talk_base::AsyncSocket* cricket_socket_;
 
 class TXmppSocket: public buzz::AsyncSocket, public sigslot::has_slots<> {
  public:
-  TXmppSocket(buzz::TlsOptions tls);
+  explicit TXmppSocket(buzz::TlsOptions tls);
   ~TXmppSocket();
 
   virtual buzz::AsyncSocket::State state();
