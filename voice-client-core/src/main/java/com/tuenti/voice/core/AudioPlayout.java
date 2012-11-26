698dc1bb947cad192d640c4166b66ff4df9b4c79
lukeweber <luke.weber@gmail.com>
2012-11-26 11:38:34 +0100

    Audio playout event handler

---
+++ voice-client-core/src/main/java/com/tuenti/voice/core/AudioPlayout.java
@@ -0,0 +1,13 @@
+package com.tuenti.voice.core;
+
+public enum AudioPlayout
+{
+    ENABLED;
+
+// -------------------------- STATIC METHODS --------------------------
+
+    public static AudioPlayout fromInteger( int i )
+    {
+        return ENABLED;
+    }
+}
