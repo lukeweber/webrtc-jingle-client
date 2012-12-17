b9edcfa28d4b14683f38a0d67ee1943f4030372c
Johann Reyes <jreyes@vaporwarecorp.com>
2012-12-17 09:19:19 -0500

    Added support for accelerometer

--- android/voice-client-core/src/main/java/com/tuenti/voice/core/util/WakeLockUtil.java
+++ android/voice-client-core/src/main/java/com/tuenti/voice/core/util/WakeLockUtil.java
@@ -7,15 +7,19 @@ import android.util.Log;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
+import static com.tuenti.voice.core.util.AccelerometerListener.ORIENTATION_HORIZONTAL;
+import static com.tuenti.voice.core.util.AccelerometerListener.OrientationListener;
+
 public final class WakeLockUtil
+    implements OrientationListener
 {
 // ------------------------------ FIELDS ------------------------------
 
     private static final String TAG = "WakeLockUtil";
 
-    private PowerManager mPowerManager;
+    private AccelerometerListener mAccelerometerListener;
 
-    private boolean mProximityEnabled;
+    private boolean mProximityLockStarted;
 
     private PowerManager.WakeLock mProximityWakeLock;
 
@@ -23,33 +27,49 @@ public final class WakeLockUtil
 
     public WakeLockUtil( final Context context )
     {
-        mPowerManager = (PowerManager) context.getSystemService( Context.POWER_SERVICE );
-        initProximity();
+        initProximity( context );
+        initAccelerometer( context );
+    }
+
+// ------------------------ INTERFACE METHODS ------------------------
+
+// --------------------- Interface OrientationListener ---------------------
+
+    @Override
+    public void orientationChanged( int orientation )
+    {
+        if ( mProximityLockStarted )
+        {
+            updateProximitySensorMode( orientation != ORIENTATION_HORIZONTAL );
+        }
     }
 
 // -------------------------- OTHER METHODS --------------------------
 
     public void startProximityLock()
     {
-        if ( mProximityWakeLock != null && !mProximityWakeLock.isHeld() )
+        updateProximitySensorMode( true );
+        if ( mAccelerometerListener != null )
         {
-            Log.d( TAG, "updateProximitySensorMode: acquiring..." );
-            mProximityWakeLock.acquire();
+            mAccelerometerListener.enable( true );
         }
+
+        mProximityLockStarted = true;
     }
 
     public void stopProximityLock()
     {
-        if ( mProximityWakeLock != null && mProximityWakeLock.isHeld() )
+        updateProximitySensorMode( false );
+        if ( mAccelerometerListener != null )
         {
-            Log.d( TAG, "updateProximitySensorMode: releasing..." );
-            mProximityWakeLock.release();
+            mAccelerometerListener.enable( false );
         }
+        mProximityLockStarted = false;
     }
 
     public boolean supportsProximity()
     {
-        return mProximityEnabled;
+        return ( mProximityWakeLock != null );
     }
 
     private int getFlag( String key )
@@ -69,23 +89,33 @@ public final class WakeLockUtil
         return 0;
     }
 
-    private void initProximity()
+    private void initAccelerometer( final Context context )
     {
+        if ( supportsProximity() )
+        {
+            mAccelerometerListener = new AccelerometerListener( context, this );
+        }
+    }
+
+    private void initProximity( final Context context )
+    {
+        PowerManager mPowerManager = (PowerManager) context.getSystemService( Context.POWER_SERVICE );
         int proximityScreenOffWakeLock = getFlag( "PROXIMITY_SCREEN_OFF_WAKE_LOCK" );
         if ( proximityScreenOffWakeLock > 0 )
         {
+            boolean proximityLockSupported = false;
             try
             {
                 if ( Compatibility.isCompatible( 17 ) )
                 {
                     Method method = mPowerManager.getClass().getDeclaredMethod( "isWakeLockLevelSupported", int.class );
-                    mProximityEnabled = (Boolean) method.invoke( mPowerManager, proximityScreenOffWakeLock );
+                    proximityLockSupported = (Boolean) method.invoke( mPowerManager, proximityScreenOffWakeLock );
                 }
                 else
                 {
                     Method method = mPowerManager.getClass().getDeclaredMethod( "getSupportedWakeLockFlags" );
                     int supportedWakeLockFlags = (Integer) method.invoke( mPowerManager );
-                    mProximityEnabled = ( ( supportedWakeLockFlags & proximityScreenOffWakeLock ) != 0x0 );
+                    proximityLockSupported = ( ( supportedWakeLockFlags & proximityScreenOffWakeLock ) != 0x0 );
                 }
             }
             catch ( Exception e )
@@ -93,9 +123,29 @@ public final class WakeLockUtil
                 Log.d( TAG, e.getMessage(), e );
             }
 
-            if ( mProximityEnabled )
+            if ( proximityLockSupported )
             {
                 mProximityWakeLock = mPowerManager.newWakeLock( proximityScreenOffWakeLock, TAG );
+            }
+        }
+    }
+
+    private void updateProximitySensorMode( boolean enabled )
+    {
+        if ( mProximityWakeLock != null )
+        {
+            synchronized ( this )
+            {
+                if ( enabled && !mProximityWakeLock.isHeld() )
+                {
+                    Log.d( TAG, "updateProximitySensorMode: acquiring..." );
+                    mProximityWakeLock.acquire();
+                }
+                if ( !enabled && mProximityWakeLock.isHeld() )
+                {
+                    Log.d( TAG, "updateProximitySensorMode: releasing..." );
+                    mProximityWakeLock.release();
+                }
             }
         }
     }
