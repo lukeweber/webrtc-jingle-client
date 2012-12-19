d53421f60d4485ad2178a55265ea27c4bd54f205
Nick Flink <nickflink@github.com>
2012-12-19 17:05:38 +0100

    some fixes for ipad story board and logging

--- client/logging.h
+++ client/logging.h
@@ -32,6 +32,9 @@
 #define LOG_TAG "c-libjingle-webrtc"  // As in WEBRTC Native...
 #define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#elif LOGGING && IOS
+#define LOGI(...) printf(__VA_ARGS__);printf("\n")
+#define LOGE(...) printf(__VA_ARGS__);printf("\n")
 #else
 #define LOGI(...) (void)0
 #define LOGE(...) (void)0
