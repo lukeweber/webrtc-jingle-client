2d5ffcfcf8dec6804afce3e7a14c51b2482af508
Luke Weber <luke.weber@gmail.com>
2013-04-10 17:03:06 +0200

    - Adding Neon libs to ARMv7 build - Supressing openssl warnings(-w) - Reverting to using gcc instead of clang on release and final builds - Removing mips from release and final builds because -icf=safe is not supported and it's a pain to build it conditionally at an app level without more work. Ticket has been filed here: https://code.google.com/p/android/issues/detail?id=54146

--- android/voice-client-core/jni/default_release.mk
+++ android/voice-client-core/jni/default_release.mk
@@ -7,21 +7,21 @@
 # be found in the AUTHORS file in the root of the source tree.
 
 # Build both ARMv5TE and ARMv7-A x86 and mips machine code.
-NDK_TOOLCHAIN_VERSION=clang3.1
 APP_ABI := armeabi armeabi-v7a
 APP_PLATFORM := android-9
 APP_CPPFLAGS += \
 	-fno-rtti -ffast-math -O3
+
 APP_CFLAGS += \
 	-DENABLE_DEBUG=0 \
 	-DLOGGING=0 \
 	-fvisibility=hidden \
 	-ffunction-sections  \
 	-fdata-sections
-APP_LDFLAGS += -Wl,--gc-sections
-ifneq ($(TARGET_ARCH),mips)
-	APP_LDFLAGS += -Wl,--icf=safe
-endif
+
+APP_LDFLAGS += -Wl,--gc-sections \
+			   -Wl,--icf=safe
+
 APP_STL := stlport_static
 APP_OPTIM := release
 ENABLE_WEBRTC_TRACE := 0
--- android/voice-client-core/jni/tuenti_final.mk
+++ android/voice-client-core/jni/tuenti_final.mk
@@ -5,8 +5,16 @@
 # tree. An additional intellectual property rights grant can be found
 # in the file PATENTS.  All contributing project authors may
 # be found in the AUTHORS file in the root of the source tree.
-NDK_TOOLCHAIN_VERSION=clang3.1
-APP_ABI := armeabi armeabi-v7a x86 mips
+APP_ABI := armeabi armeabi-v7a x86
+#Crash in optimized neon code with clang.
+#NDK_TOOLCHAIN_VERSION=clang3.1
+#Mips temporarily not building
+#Below code has no effect as TARGET_ARCH is not defined at this level
+#FILE is only parsed one time.
+#ifneq ($(TARGET_ARCH),mips)
+#	APP_LDFLAGS += -Wl,--icf=safe
+#endif
+
 APP_PLATFORM := android-9
 APP_CPPFLAGS += \
 	-fno-rtti -ffast-math -O3
@@ -19,9 +27,7 @@ APP_CFLAGS += \
 	-ffunction-sections  \
 	-fdata-sections
 APP_LDFLAGS += -Wl,--gc-sections
-ifneq ($(TARGET_ARCH),mips)
-	APP_LDFLAGS += -Wl,--icf=safe
-endif
+APP_LDFLAGS += -Wl,--icf=safe
 APP_STL := stlport_static
 APP_OPTIM := release
 ENABLE_WEBRTC_TRACE := 0
